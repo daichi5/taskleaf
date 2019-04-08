9198af8541e54d023af9b20145668cecc32ae6fc
daichi <sw41dc@gmail.com>
2019-04-08 18:15:24 +0900

    modify admin_user views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,19 +7,32 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_users_path, notice: 'ユーザー「#{@user.name}」を登録しました'
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました"
     else
       render :new
     end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
   end
 
   private
